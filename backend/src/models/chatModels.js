23afceadadedac4b9964db51894552da196be9d0
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2025-02-20 13:15:00 +0200

    added checking for the present of variables

--- backend/src/models/chatModels.js
+++ backend/src/models/chatModels.js
@@ -3,6 +3,10 @@ import { ChatOpenAI } from '@langchain/openai';
 
 dotenv.config();
 
+if (!process.env.OPENAI_API_KEY) {
+  throw new Error('Missing OPENAI_API_KEY in environment variables');
+}
+
 export const model = new ChatOpenAI({
   model: 'gpt-4o-mini',
   temperature: 0,
