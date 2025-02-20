013f32dd9edbd9d4b438ebd01796fab89ce74124
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2025-02-20 12:14:13 +0200

    added check if the server is running

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,4 +11,9 @@ app.use(express.json());
 
 app.use('/chat', router);
 
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send('Something went wrong!');
+});
+
 app.listen(3001, () => console.log('Server running'));
