825816aedf5227cbab7588300b8f4d5dcb811fc1
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2025-03-12 16:40:52 +0200

    added internationalization settings

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+const SUPPORTED_LANGUAGES = ["uk", "en", "es", "fr", "de"];
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  i18n: {
+    locales: SUPPORTED_LANGUAGES,
+    defaultLocale: "uk",
+  },
 };
 
 export default nextConfig;
---
+++ frontend/src/app/[lang]/dictionaries.ts
@@ -0,0 +1,12 @@
+import "server-only";
+
+const dictionaries = {
+  uk: () => import("./dictionaries/uk.json").then((module) => module.default),
+  en: () => import("./dictionaries/en.json").then((module) => module.default),
+  es: () => import("./dictionaries/es.json").then((module) => module.default),
+  fr: () => import("./dictionaries/fr.json").then((module) => module.default),
+  de: () => import("./dictionaries/de.json").then((module) => module.default),
+};
+
+export const getDictionary = async (locale: "uk" | "en" | "es" | "fr" | "de") =>
+  dictionaries[locale]();
